ee25b9f8d1d283121037e521667cbd61517269ab
yuan yuan <y.yuan@berkeley.edu>
2015-08-22 00:18:57 -0700

    added link to projects on index.html, beginning masonry package

---
+++ assets/js/projects.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  // init Masonry
+  $('.grid').masonry({
+    //options
+    itemSelector: '.grid-item',
+    columnWidth: 120,
+    isFitWidth: true 
+  });
+
+  
+})
