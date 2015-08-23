c208276346f660b94cbcf558d03467c8e378f436
yuan yuan <y.yuan@berkeley.edu>
2015-08-23 00:10:31 -0700

    projects layout in place

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,14 +1,35 @@
 $(document).ready( function() {
+
   var $grid = $('.grid').masonry({
       itemSelector: '.grid-item',
       isFitWidth: true,
       columnWidth: '.grid-sizer',
-      gutter: 10
+      gutter: 20 
     }); 
 
   // masonry init
   $('.grid').imagesLoaded(function() {
     $grid.masonry();  
+  });
+
+  // fancybox init
+  $(".fancybox").fancybox();
+
+  $(".fancybox").fancybox({
+      beforeLoad: function() {
+        this.title = $(this.element).children(".caption")[0].innerHTML;
+      },
+      openEffect  : 'elastic',
+      closeEffect : 'elastic',
+      helpers : {
+        title : {
+          type : 'inside'
+        }
+      },
+      afterShow: function() {
+        var link = $(this.element).children(".link")[0].innerHTML;
+        $(".fancybox-image").wrap('<a href="' + link + '" />')
+      },   
   });
 });
 
